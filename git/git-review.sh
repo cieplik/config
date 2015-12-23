1b46cb42db78d399bfe3ae3cf7ca27dab5da6d13
cieplik <dcieplin@gmail.com>
2015-12-23 10:28:54 -0500

    Git tweaks

--- git/git-review.sh
+++ git/git-review.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-  cat <<EOF
-USAGE: $(basename "$0") [REVSPEC...]
-REVSPEC tricks:
-  --cached/--staged   - files to be committed
-  REV^!               - changes in particular commit (REV)
-  master_ci...HEAD    - changes from last common ancestor with 'master_ci'
-EOF
+  me=$(basename "$0")
+  echo "USAGE: $me [REVSPEC...]"
+  echo "REVSPEC tricks:"
+  echo "  --cached/--staged   - files to be committed"
+  echo "  REV^!               - changes in particular commit (REV)"
+  echo "  master_ci...HEAD    - changes from last common ancestor with 'master_ci'"
   exit
 fi
-revs="$1"
 
-gitroot="$(git rev-parse --show-toplevel)"
-cd "$gitroot"
+cd "$(git rev-parse --show-toplevel)"                      # cd to repo root dir
 
 dialog="$(which dialog whiptail 2>/dev/null | head -n1)"
 [ -n "$dialog" ] || {
@@ -24,22 +21,20 @@ dialog="$(which dialog whiptail 2>/dev/null | head -n1)"
 # build list of differing files in correct format for 'dialog --menu'
 menufiles=($(
   git diff --name-only "$@" -- |  # list files differing between specified revs,
-    grep -vE '^.gitcc$' |         # filter out '.gitcc',
     nl                            # prepend index number to each filename
 )) || exit 1
 
-option=1
 while true; do
   option=$(
-    "$dialog" --default-item "$option" --menu \
-      "Files changed for \"$@\":" \
-      $(( $(tput lines)-5 )) $(( $(tput cols)-10 )) \
-      $(( $(tput lines)-12 )) \
-      "${menufiles[@]}" \
+    "$dialog" --default-item "${option:-1}" --menu                             \
+      "Files changed for \"$@\":"                                              \
+      $(( $(tput lines)-5 )) $(( $(tput cols)-10 ))                            \
+      $(( $(tput lines)-12 ))                                                  \
+      "${menufiles[@]}"                                                        \
       3>&1 1>&2 2>&3
   )
   [[ -n "$option" ]] || break
 
-  git difftool -y --tool=vimdiff "$@" -- "${menufiles[$((2*option-1))]}" || exit 1
+  git difftool -y "$@" -- "${menufiles[$((2*option-1))]}" || exit 1
 done
 
